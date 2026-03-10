3d27e0d0ba76bb3db9eca4c9f5b709e848b48d8d
Rainer Paja <your_github_email_here>
2026-03-10 15:39:30 +0800

    Refactor Office Migration

--- database/migrations/2026_03_10_062503_create_offices_table.php
+++ database/migrations/2026_03_10_062503_create_offices_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('offices', function (Blueprint $table) {
-            $table->bigIncrements('office_id');
+            $table->id();
             $table->string('name');
             $table->string('acronym');
             $table->timestamps();
