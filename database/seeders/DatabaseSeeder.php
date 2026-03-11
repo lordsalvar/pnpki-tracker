d173a5f9bb612c99e9ffa47b44595c5c298c0b33
lordsalvar <lordcigelsalvar@gmail.com>
2026-03-11 14:38:48 +0800

    Fix user creation in DatabaseSeeder by standardizing admin name casing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,18 +19,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-<<<<<<< HEAD
-      DB::table('users')->insert([
-        'name' => 'Admin',
-        'email' => 'admin@local.dev',
-        'password' => Hash::make('password'),
-      ]);
-=======
         DB::table('users')->insert([
             'name' => 'admin',
             'email' => 'admin@local.dev',
             'password' => Hash::make('password'),
         ]);
->>>>>>> 16d7a6256cf510e02a5a4745e0e63526e33ee61a
     }
 }
