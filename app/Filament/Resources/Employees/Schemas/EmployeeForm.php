8b6ca5421d8b6e1556dae1b989d7211de2705fe7
Ernest-C <ernestjcc@gmail.com>
2026-03-11 10:41:49 +0800

    Refactor EmployeeForm to enhance address grouping

--- app/Filament/Resources/Employees/Schemas/EmployeeForm.php
+++ app/Filament/Resources/Employees/Schemas/EmployeeForm.php
@@ -5,14 +5,16 @@ namespace App\Filament\Resources\Employees\Schemas;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
-use Filament\Schemas\Components\Fieldset;
+use Filament\Schemas\Components\Group;
 
 class EmployeeForm
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(2)
             ->components([
+
                 TextInput::make('firstname')
                     ->label('First Name')
                     ->required()
@@ -26,7 +28,7 @@ class EmployeeForm
                     ->maxLength(255),
                 TextInput::make('suffix')
                     ->label('Suffix')
-                    ->placeholder('e.g. Jr., Sr., III')
+                    ->placeholder('Jr., Sr., III')
                     ->maxLength(20),
                 TextInput::make('email')
                     ->label('Email Address')
@@ -39,64 +41,64 @@ class EmployeeForm
                     ->tel()
                     ->required()
                     ->maxLength(20),
-
-                Fieldset::make('Address')
+                    
+                    
+                    Group::make()
                     ->relationship('address')
+                    ->columnSpan(2)
+                    ->columns(2)
                     ->schema([
                         TextInput::make('house_no')
-                            ->label('House No.')
-                            ->required()
-                            ->maxLength(255),
+                        ->label('House No.')
+                        ->required()
+                        ->maxLength(255),
                         TextInput::make('street')
-                            ->label('Street')
-                            ->required()
+                        ->label('Street')
+                        ->required()
                             ->maxLength(255),
-                        TextInput::make('barangay')
+                            TextInput::make('barangay')
                             ->label('Barangay')
                             ->required()
                             ->maxLength(255),
-                        TextInput::make('municipality')
+                            TextInput::make('municipality')
                             ->label('Municipality')
                             ->required()
                             ->maxLength(255),
-                        TextInput::make('province')
+                            TextInput::make('province')
                             ->label('Province')
                             ->required()
                             ->maxLength(255),
-                        TextInput::make('zip_code')
+                            TextInput::make('zip_code')
                             ->label('Zip Code')
                             ->required()
                             ->maxLength(10),
-                    ]),
+                            ]),
+                            Select::make('office_id')
+                            ->label('Office')
+                            ->relationship('office', 'name')
+                            ->getOptionLabelFromRecordUsing(fn ($record) =>
+                           $record->acronym )
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+                            TextInput::make('organizational_unit')
+                            ->label('Organizational Unit')
+                            ->required()
+                            ->maxLength(255),
+                            Select::make('gender')
+                            ->label('Gender')
+                                ->options([
+                                    'male'   => 'Male',
+                                    'female' => 'Female',
+                                    'other'  => 'Other',
+                                ])
+                                ->required(),
+                            TextInput::make('tin_number')
+                                ->label('TIN Number')
+                                ->required()
+                                ->unique(ignoreRecord: true)
+                                ->maxLength(20),
                     
-
-
-                Select::make('office_id')
-                    ->label('Office')
-                    ->relationship('office', 'name')
-                    ->getOptionLabelFromRecordUsing(fn ($record) =>
-                        "{$record->name} ({$record->acronym})"
-                    )
-                    ->searchable()
-                    ->preload()
-                    ->required(),
-                TextInput::make('organizational_unit')
-                    ->label('Organizational Unit')
-                    ->required()
-                    ->maxLength(255),
-                Select::make('gender')
-                    ->label('Gender')
-                    ->options([
-                        'male'   => 'Male',
-                        'female' => 'Female',
-                        'other'  => 'Other',
-                    ])
-                    ->required(),
-                TextInput::make('tin_number')
-                    ->label('TIN Number')
-                    ->required()
-                    ->unique(ignoreRecord: true)
-                    ->maxLength(20),
             ]);
     }
-}
+    }
