9121370fa72b85e062f5928ddbc925f0694553a7
Ernest-C <ernestjcc@gmail.com>
2026-03-12 15:02:28 +0800

    Implement address data handling in EditEmployee page

--- app/Filament/Resources/Employees/Pages/EditEmployee.php
+++ app/Filament/Resources/Employees/Pages/EditEmployee.php
@@ -5,7 +5,7 @@ namespace App\Filament\Resources\Employees\Pages;
 use App\Filament\Resources\Employees\EmployeeResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
-
+use App\Models\Address;
 class EditEmployee extends EditRecord
 {
     protected static string $resource = EmployeeResource::class;
@@ -15,5 +15,41 @@ class EditEmployee extends EditRecord
         return [
             DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $address = Address::find($data['address_id']);
+
+        if ($address) {
+            $data['house_no']     = $address->house_no;
+            $data['street']       = $address->street;
+            $data['barangay']     = $address->barangay;
+            $data['municipality'] = $address->municipality;
+            $data['province']     = $address->province;
+            $data['zip_code']     = $address->zip_code;
+        }
+
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+      
+
+        if ($this->record->address) {
+            $this->record->address->update([
+                'house_no'     => $data['house_no'],
+                'street'       => $data['street'],
+                'barangay'     => $data['barangay'],
+                'municipality' => $data['municipality'],
+                'province'     => $data['province'],
+                'zip_code'     => $data['zip_code'],
+            ]);
+        }
+
+        unset($data['house_no'], $data['street'], $data['barangay'], $data['municipality'], $data['province'], $data['zip_code']);
+
+        return $data;
     }
 }
