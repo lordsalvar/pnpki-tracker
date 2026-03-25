7b23297503fc791cc31507ec8231112f1a204f74
ernestjcc-pm <ernestcanlas@g.cjc.edu.ph>
2026-03-25 10:12:50 +0800

    Add 'Request Modification' action to ViewBatch with status update notification

--- app/Filament/Resources/Batches/Pages/ViewBatch.php
+++ app/Filament/Resources/Batches/Pages/ViewBatch.php
@@ -3,15 +3,15 @@
 namespace App\Filament\Resources\Batches\Pages;
 
 use App\Actions\FinalizeBatchAction;
+use App\Enums\ApplicationStatus;
 use App\Enums\BatchStatus;
-use App\Exports\FormSubmissionExport;
+use App\Enums\UserRole;
 use App\Filament\Resources\Batches\BatchResource;
-use App\Models\FormSubmission;
 use Filament\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
 use Illuminate\Contracts\Support\Htmlable;
-use Maatwebsite\Excel\Facades\Excel;
-
+use Illuminate\Support\Facades\Auth;
 
 class ViewBatch extends ViewRecord
 {
@@ -36,6 +36,28 @@ class ViewBatch extends ViewRecord
                 ->action(function () {
                     app(FinalizeBatchAction::class)->execute($this->record);
                     $this->refreshFormData(['status']);
+                }),
+            Action::make('request_modification')
+                ->label('Request Modification')
+                ->icon('heroicon-o-pencil-square')
+                ->color('warning')
+                ->requiresConfirmation()
+                ->modalHeading('Request Modification')
+                ->modalDescription('This will mark the application as Modification Requested.')
+                ->visible(fn () => Auth::user()?->role === UserRole::REPRESENTATIVE->value
+                    && $this->record->status === BatchStatus::FINALIZED
+                    && $this->record->application_status !== ApplicationStatus::MODIFICATION_REQUESTED)
+                ->action(function () {
+                    $this->record->update([
+                        'application_status' => ApplicationStatus::MODIFICATION_REQUESTED->value,
+                    ]);
+
+                    $this->refreshFormData(['application_status']);
+
+                    Notification::make()
+                        ->title('Application status updated to Modification Requested.')
+                        ->success()
+                        ->send();
                 }),
         ];
     }
